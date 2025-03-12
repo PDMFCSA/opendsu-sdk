2fe96654fdfcbeca9c006fa8f28236e93004dd27
vnogueira <vitor.nogueira@pdmfc.com>
2025-03-12 18:09:04 +0000

    TRUST-407 - query logic

--- modules/loki-enclave-facade/services/utils.js
+++ modules/loki-enclave-facade/services/utils.js
@@ -1,3 +1,5 @@
+const {DBKeys} = require("../utils/constants")
+
 /**
  *
  * @param {{client: nano, config: config}} self
@@ -42,7 +44,83 @@ function testErrorForShutdown(error, logger){
 }
 
 
+let filterOperationsMap = {
+    "!=": "$ne",
+    "==": "$eq",
+    ">": "$gt",
+    ">=": "$gte",
+    "<": "$lt",
+    "<=": "$lte",
+    "like": "$regex"
+}
+
+/**
+ * @description Converts OpenDSU query syntax to Couchdb mango query
+ * @param {string[]} conditions - The conditions to filter by
+ * @param {[string, "asc" | "desc"]} sort - The sorting criteria.
+ * @param {number} [limit] the query limit. defaults to 250
+ *
+ **/
+function parseConditionsToDBQuery(conditions, sort, limit = 250) {
+    const mQuery = {
+        selector: {},
+        limit: limit
+    }
+
+    if (sort){
+        mQuery.sort = {[sort[0]]: sort[1]}
+    }
+
+    if (!conditions || conditions.length === 0 || conditions === "") {
+        if (mQuery.sort) {
+            mQuery.selector[sort[0]] = {"$gt": null};
+        } else {
+            mQuery.selector = {[DBKeys.PK]: {"$gt": null}};
+        }
+    } else {
+        let isSortIncluded = false;
+        conditions.forEach(condition => {
+            // Update regex pattern to capture more complex patterns for LIKE
+            const match = condition.match(/^(\w+)\s*(>=|<=|==|!=|<>|>|<|like)\s*(.*)$/i);
+            if (!match) {
+                throw new Error(`Invalid condition: ${condition}`);
+            }
+
+            const [, field, operator, value] = match;
+            if (field === sort[0]) {
+                isSortIncluded = true;
+            }
+            const couchOperator = filterOperationsMap[operator.toLowerCase()];
+
+            let conditionObject = {};
+
+            if (operator.toLowerCase() === 'like') {
+                // Process LIKE condition, and allow complex regex patterns (no quotes required)
+                conditionObject[field] = {[couchOperator]: new RegExp(value.trim(), 'i')}; // case-insensitive regex
+            } else {
+
+                // Process other operators, handling numeric and string cases
+                const numericValue = /^[0-9]+$/.test(value) ? parseFloat(value) : value;
+                conditionObject[field] = {
+                    [couchOperator]: isNaN(numericValue) ? value.replace(/['"]/g, '').trim() : numericValue
+                };
+            }
+
+            mQuery.selector[field] = conditionObject;
+        });
+        if (!isSortIncluded && mQuery.sort) {
+            mQuery.selector[sort[0]] = {"$gt": null};
+        }
+    }
+
+    mQuery.selector = {$and: mQuery.selector};
+
+    return mQuery;
+}
+
+
 module.exports =  {
+    parseConditionsToDBQuery,
     testErrorForShutdown,
     ensureAuth
 }
