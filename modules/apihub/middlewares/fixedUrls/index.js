bcfa7efbb74a327f1fc494b7b4ec631f879b00f7
vnogueira <vitor.nogueira@pdmfc.com>
2025-03-18 18:36:07 +0000

    TRUST-407 - more magic

--- modules/apihub/middlewares/fixedUrls/index.js
+++ modules/apihub/middlewares/fixedUrls/index.js
@@ -471,12 +471,12 @@ module.exports = function (server) {
                             logger.error("Failed to grant write access to the enclave", err.message, err.code, err.rootCause);
                         }
 
-                        lightDBEnclaveClient.createCollection($$.SYSTEM_IDENTIFIER, TASKS_TABLE, ["pk", "timestamp", "url"], (err) => {
+                        lightDBEnclaveClient.createCollection($$.SYSTEM_IDENTIFIER, TASKS_TABLE, ["pk", "__timestamp", "url"], (err) => {
                             if (err) {
                                 logger.error("Failed to create collection", err.message, err.code, err.rootCause);
                             }
 
-                            lightDBEnclaveClient.createCollection($$.SYSTEM_IDENTIFIER, HISTORY_TABLE, ["pk", "timestamp", "url"], (err) => {
+                            lightDBEnclaveClient.createCollection($$.SYSTEM_IDENTIFIER, HISTORY_TABLE, ["pk", "__timestamp", "url"], (err) => {
                                 if (err) {
                                     logger.error("Failed to create collection", err.message, err.code, err.rootCause);
                                 }
